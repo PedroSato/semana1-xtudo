d67ba3380d1a7d9b6f171f3ba5ab46118d1ab683
Pedro Sato <pedro.hiro@gmail.com>
2019-06-20 12:03:21 -0300

    minor changes regarding article responsiveness

--- easyCar/src/Helpers/DateFormatter.js
+++
@@ -1,6 +0,0 @@
-const getDate = () =>{
-    const date = new Date();
-    const formatted = `${date.getFullYear()}-${date.getMonth()}-${date.getDay()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
-    return formatted
-}
-export default getDate()
